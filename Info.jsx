4d5fd8568d8df93f18892d1b3b5959dfa51fb2ea
Rajvi03 <82755569+Rajvi03@users.noreply.github.com>
2021-06-28 12:43:59 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        <h1>GRAPHICS DESIGNER</h1>
+      </div>
+      <div className="note">
+        <h1>LOGO DESIGNER</h1>
+      </div>
+      <div className="note">
+        <h1>PHOTO EDITOR</h1>
+      </div>
+      <div className="note1">
+        <h1>Festival & Greeting Poster</h1>
+      </div>
+      <div className="note1">
+        <h1> Product Promotions</h1>
+      </div>
+      <div className="note1">
+        <h1> Instagram stories</h1>
+      </div>
+      <div className="note1">
+        <h1>WhatsApp status</h1>
+      </div>
+      <div className="note1">
+        <h1>Facebook & YouTube cover</h1>
+      </div>
+      <div className="note1">
+        <h1>Video Editing</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
